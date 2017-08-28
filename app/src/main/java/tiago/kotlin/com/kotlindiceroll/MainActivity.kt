19f2e907e7dd172a32e91c623c02f4ca5ad4bf2a
tiagoalexbastos <tiagoalexbastos@gmail.com>
2017-08-28 16:09:14 +0100

    Changed to ConstraintLayout

--- app/src/main/java/tiago/kotlin/com/kotlindiceroll/MainActivity.kt
+++ app/src/main/java/tiago/kotlin/com/kotlindiceroll/MainActivity.kt
@@ -2,7 +2,6 @@ package tiago.kotlin.com.kotlindiceroll
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -10,7 +9,6 @@ class MainActivity : AppCompatActivity() {
 
     private val max = 6
     private val min = 1
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
